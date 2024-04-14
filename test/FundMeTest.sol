b912c0a04ac6e83ae2b5eab8b237ce0c09e9acd5
okorieebube <okorieebube1@gmail.com>
2024-04-14 18:07:39 +0100

    fund me contract

---
+++ test/FundMeTest.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external{
+        fundMe = new FundMe();
+    }
+
+    function testMinUsdValueIsFive() public view{
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+
+    
+}
